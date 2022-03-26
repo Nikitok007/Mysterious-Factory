662584cb05e19ce6a593237dfd88602801c88509
nikit <ivanomsk7@gmail.com>
2022-03-26 18:23:42 +0600

    init

---
+++ desktop/src/ru/konbgroupe/mysteriousfactory/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.konbgroupe.mysteriousfactory.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.konbgroupe.mysteriousfactory.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
